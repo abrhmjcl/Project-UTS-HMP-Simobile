8c2cf772795f3c2d2a40ed8a6ecc925e1f969915
abrhmjcl <abamjeco4563@gmail.com>
2026-09-13 19:31:51 +0700

    tambah dark mode

--- SIMOBILE-UTS/src/app/pages/settings/settings.page.ts
+++ SIMOBILE-UTS/src/app/pages/settings/settings.page.ts
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
+  isDarkMode: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.isDarkMode = document.body.classList.contains('dark');
   }
 
+  toggleDarkMode(event: any) {
+    const isChecked = event.detail.checked;
+    document.body.classList.toggle('dark', isChecked);
+  }
 }
