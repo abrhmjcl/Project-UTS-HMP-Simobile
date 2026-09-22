70c2b461c7d245a5c2bb49e61b649c1cacec7132
Xiscoo_22 <s160424106@student.ubaya.ac.id>
2026-09-22 20:32:26 +0700

    Implementasi Product Form Page dengan reaktif form dan validasi

--- SIMOBILE-UTS/src/app/pages/product-form/product-form.module.ts
+++ SIMOBILE-UTS/src/app/pages/product-form/product-form.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular/lazy';
 
 import { ProductFormPageRoutingModule } from './product-form-routing.module';
-
 import { ProductFormPage } from './product-form.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     ProductFormPageRoutingModule
   ],
--- SIMOBILE-UTS/src/app/pages/product-form/product-form.page.ts
+++ SIMOBILE-UTS/src/app/pages/product-form/product-form.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-form',
@@ -7,10 +11,82 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ProductFormPage implements OnInit {
+  productForm!: FormGroup;
+  isEditMode: boolean = false;
+  productIdToEdit: number | null = null;
+  
+  categories: string[] = ['Makanan Pokok', 'Makanan Instan', 'Minuman', 'Bumbu Dapur', 'Kebutuhan Rumah', 'Snack', 'Lainnya'];
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      category: ['', [Validators.required]],
+      description: [''],
+      purchasePrice: ['', [Validators.required, Validators.min(1)]],
+      sellingPrice: ['', [Validators.required, Validators.min(1)]],
+      stock: ['', [Validators.required, Validators.min(0)]],
+      imageUrl: ['']
+    });
+
+    this.route.queryParams.subscribe(params => {
+      if (params['id']) {
+        this.isEditMode = true;
+        this.productIdToEdit = parseInt(params['id'], 10);
+        this.loadProductForEdit(this.productIdToEdit);
+      }
+    });
   }
 
+  get f() {
+    return this.productForm.controls;
+  }
+
+  loadProductForEdit(id: number) {
+    const product = this.productService.getProductById(id);
+    if (product) {
+      this.productForm.patchValue({
+        name: product.name,
+        category: product.category,
+        description: product.description,
+        purchasePrice: product.purchasePrice,
+        sellingPrice: product.sellingPrice,
+        stock: product.stock,
+        imageUrl: product.imageUrl
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.productForm.invalid) {
+      this.productForm.markAllAsTouched();
+      return;
+    }
+
+    const formData = this.productForm.value;
+    
+    if (this.isEditMode && this.productIdToEdit) {
+      const updatedProduct: Product = {
+        id: this.productIdToEdit,
+        ...formData
+      };
+      // this.productService.updateProduct(updatedProduct);
+      console.log('Update Product:', updatedProduct);
+    } else {
+      const newProduct: Product = {
+        id: new Date().getTime(),
+        ...formData
+      };
+      // this.productService.addProduct(newProduct);
+      console.log('Add Product:', newProduct);
+    }
+
+    this.router.navigate(['/tabs/products']);
+  }
 }
