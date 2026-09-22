e884c26d312c5c69fe7ac8cffbfe814c9971887f
Xiscoo_22 <s160424106@student.ubaya.ac.id>
2026-09-22 20:30:25 +0700

    Implementasi Product Detail Page dengan property dan Event Binding

--- SIMOBILE-UTS/src/app/pages/product-detail/product-detail.page.ts
+++ SIMOBILE-UTS/src/app/pages/product-detail/product-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ProductDetailPage implements OnInit {
+  product: Product | undefined;
+  profit: number = 0;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private cartService: CartService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      const id = parseInt(idParam, 10);
+      this.product = this.productService.getProductById(id);
+      
+      if (this.product) {
+        this.profit = this.product.sellingPrice - this.product.purchasePrice;
+      }
+    }
   }
 
+  addToCart() {
+    if (this.product && this.product.stock > 0) {
+      this.cartService.addToCart(this.product);
+      this.router.navigate(['/tabs/cart']);
+    }
+  }
+
+  editProduct() {
+    if (this.product) {
+      this.router.navigate(['/tabs/product-form'], { queryParams: { id: this.product.id } });
+    }
+  }
 }
