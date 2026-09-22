c7652a4137020b58cd15b0378d0bd8afb55417d1
Xiscoo_22 <s160424106@student.ubaya.ac.id>
2026-09-22 20:21:36 +0700

    Membuat ComponentsModule

---
+++ SIMOBILE-UTS/src/app/components/components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular/lazy';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { EmptyStateComponent } from './empty-state/empty-state.component';
+import { CustomHeaderComponent } from './custom-header/custom-header.component';
+
+@NgModule({
+  declarations: [
+    ProductCardComponent,
+    EmptyStateComponent,
+    CustomHeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule
+  ],
+  exports: [
+    ProductCardComponent,
+    EmptyStateComponent,
+    CustomHeaderComponent
+  ]
+})
+export class ComponentsModule { }
