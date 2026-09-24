ba3dc42c7da461b14181b70dcc15d3b7d6e20f2c
Xiscoo_22 <s160424106@student.ubaya.ac.id>
2026-09-24 10:08:01 +0700

    Perbaikan Punya Egi

--- SIMOBILE-UTS/src/app/pages/cart/cart.module.ts
+++ SIMOBILE-UTS/src/app/pages/cart/cart.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular/lazy';
 
 import { CartPageRoutingModule } from './cart-routing.module';
-
 import { CartPage } from './cart.page';
+// Import jika ada ComponentsModule: import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
@@ -14,6 +13,7 @@ import { CartPage } from './cart.page';
     FormsModule,
     IonicModule,
     CartPageRoutingModule
+    // ComponentsModule
   ],
   declarations: [CartPage]
 })
--- SIMOBILE-UTS/src/app/pages/cart/cart.page.ts
+++ SIMOBILE-UTS/src/app/pages/cart/cart.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular/lazy';
+import { CartService } from '../../services/cart.service';
+import { ProductService } from '../../services/product.service';
+import { TransactionService } from '../../services/transaction.service';
+import { CartItem } from '../../models/cart-item.model';
 
 @Component({
   selector: 'app-cart',
@@ -6,11 +12,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
   standalone: false,
 })
-export class CartPage implements OnInit {
+export class CartPage {
+  cartItems: CartItem[] = [];
+  cartTotal: number = 0;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService,
+    private transactionService: TransactionService,
+    private router: Router,
+    private alertController: AlertController
+  ) { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.loadCart();
   }
 
+  loadCart() {
+    this.cartItems = this.cartService.getCartItems();
+    this.cartTotal = this.cartService.getCartTotal();
+  }
+
+  increaseQty(productId: number) {
+    const item = this.cartItems.find(i => i.product.id === productId);
+    if (item) {
+      this.cartService.updateQuantity(productId, item.quantity + 1);
+      this.loadCart();
+    }
+  }
+
+  decreaseQty(productId: number) {
+    const item = this.cartItems.find(i => i.product.id === productId);
+    if (item) {
+      this.cartService.updateQuantity(productId, item.quantity - 1);
+      this.loadCart();
+    }
+  }
+
+  removeItem(productId: number) {
+    this.cartService.removeFromCart(productId);
+    this.loadCart();
+  }
+
+  async confirmCheckout() {
+    const alert = await this.alertController.create({
+      header: 'Konfirmasi Checkout',
+      message: `Total belanja Anda Rp ${this.cartTotal.toLocaleString('id-ID')}. Lanjutkan?`,
+      buttons: [
+        {
+          text: 'Batal',
+          role: 'cancel'
+        },
+        {
+          text: 'Checkout',
+          handler: () => {
+            this.processCheckout();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  processCheckout() {
+    const transaction = this.transactionService.createTransaction(this.cartItems, this.cartTotal);
+
+    // Update stok produk
+    this.cartItems.forEach(item => {
+      const product = this.productService.getProductById(item.product.id);
+      if (product) {
+        this.productService.updateProduct(product.id, {
+          stock: product.stock - item.quantity
+        });
+      }
+    });
+
+    this.cartService.clearCart();
+    this.loadCart();
+
+    // Arahkan ke riwayat transaksi (Tab 3: Transaksi)
+    this.router.navigate(['/tabs/transactions']);
+  }
 }
